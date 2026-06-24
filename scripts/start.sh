06e3805b5e9bb4c61ea89d110d95bbd5964d4843
hoppangm3 <hoppangm3@gmail.com>
2026-06-24 12:53:17 +0900

    feat: hide JWT secret using CodeBuild environment variables

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,7 +4,12 @@ echo "Start Spring Boot application"
 # JAR 파일이 있는 위치로 이동
 cd /home/ec2-user/app/target
 
-# ec2-user 권한으로 Java 절대 경로를 이용해 백그라운드 실행
-nohup sudo -u ec2-user /usr/bin/java -jar bookapp-0.0.1-SNAPSHOT.jar > /home/ec2-user/app/app.log 2>&1 < /dev/null &
+# 환경 변수 스크립트 로드
+if [ -f /home/ec2-user/app/scripts/env.sh ]; then
+    source /home/ec2-user/app/scripts/env.sh
+fi
+
+# ec2-user 권한으로 Java 절대 경로를 이용해 백그라운드 실행 (환경 변수 유지)
+nohup sudo -E -u ec2-user /usr/bin/java -jar bookapp-0.0.1-SNAPSHOT.jar > /home/ec2-user/app/app.log 2>&1 < /dev/null &
 
 echo "Application deployment script finished"
