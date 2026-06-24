729bcf77f887e24f1a8f00fad1fab4730fe214a9
hoppangm3 <hoppangm3@gmail.com>
2026-06-24 10:58:12 +0900

    fix: change user and path from ubuntu to ec2-user for Amazon Linux

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Start Spring Boot application"
-cd /home/ubuntu/app
-nohup java -jar target/bookapp-0.0.1-SNAPSHOT.jar > /home/ubuntu/app/app.log 2>&1 &
+cd /home/ec2-user/app
+nohup java -jar target/bookapp-0.0.1-SNAPSHOT.jar > /home/ec2-user/app/app.log 2>&1 &
