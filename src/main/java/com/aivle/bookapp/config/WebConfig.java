d50095670dfb3630317e04a482dd345d4f994c78
Qlrl <kjminji01@gmail.com>
2026-06-25 08:25:43 +0900

    webconfig update v1

--- src/main/java/com/aivle/bookapp/config/WebConfig.java
+++ src/main/java/com/aivle/bookapp/config/WebConfig.java
@@ -13,7 +13,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "http://d913vpzid8uar.cloudfront.net",
+                        "https://d913vpzid8uar.cloudfront.net"
+                )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
