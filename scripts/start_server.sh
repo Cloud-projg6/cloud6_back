2ade78d5e29c70299fa8149a8cb4c812e93c2d0f
Qlrl <kjminji01@gmail.com>
2026-06-24 10:15:26 +0900

    appspec.yml, start_server.sh 경로 수정

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 echo "새 애플리케이션 배포 및 실행..."
-cd /home/ubuntu/app
-# 타겟 폴더 내의 jar 파일을 백그라운드로 실행하고 로그를 저장합니다.
-nohup java -jar target/*.jar > nohup.out 2>&1 &
+
+# Amazon Linux 경로로 이동
+cd /home/ec2-user/app
+
+# 복사된 jar 파일을 바로 백그라운드로 실행하고 로그를 저장합니다.
+nohup java -jar *.jar > nohup.out 2>&1 &
+
 echo "애플리케이션 실행 완료!"
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "기존 Spring Boot 애플리케이션 종료 중..."
-CURRENT_PID=$(pgrep -f .jar)
+
+# 실행 중인 java 프로세스 중 .jar를 포함한 프로세스의 PID를 찾습니다.
+CURRENT_PID=$(pgrep -f java)
+
 if [ -z "$CURRENT_PID" ]; then
     echo "구동 중인 애플리케이션이 없습니다."
 else
