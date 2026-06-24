d302dc0b14fa7cef02f3e910ebf45d3f60ba8ebb
surin <surinrtf@gmail.com>
2026-06-24 16:31:55 +0900

    fix error

--- deploy-scripts/start.sh
+++ deploy-scripts/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
 JAR_FILE=$(ls /home/ec2-user/app/target/*.jar | head -1)
-cd /home/ec2-user/app
-nohup java -jar $JAR_FILE > /home/ec2-user/app/app.log 2>&1 &
+nohup java -jar $JAR_FILE \
+  --spring.config.additional-location=/home/ec2-user/app/config/ \
+  > /home/ec2-user/app/app.log 2>&1 &
