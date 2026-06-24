ca68f2f7257a5b5a2741ac7375c5e3fb9cc2206d
hoppangm3 <hoppangm3@gmail.com>
2026-06-24 11:54:55 +0900

    fix: CodeDeploy nohup issue by adding stdin redirect and fix target path

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Start Spring Boot application"
-cd /home/ec2-user/app
-nohup java -jar target/bookapp-0.0.1-SNAPSHOT.jar > /home/ec2-user/app/app.log 2>&1 &
+cd /home/ec2-user/app/target
+nohup java -jar bookapp-0.0.1-SNAPSHOT.jar > /home/ec2-user/app/app.log 2>&1 < /dev/null &
