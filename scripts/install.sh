7e063b8b2abc63acf9c40422a38f58fb4417eb7f
soojung <lilly6026@naver.com>
2026-06-24 16:51:25 +0900

    fix

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# 빌드 산출물 jar를 고정 이름으로 정리
-JAR=$(ls /home/ec2-user/app/build/libs/*.jar | grep -v plain | head -n1)
-cp "$JAR" /home/ec2-user/app/app.jar
+set -e
+cp /home/ec2-user/app/deploy/app.jar /home/ec2-user/app/app.jar
