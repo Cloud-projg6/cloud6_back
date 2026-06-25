6b4e7ff85d57f01ef78e25c391cc2860e8c47294
hoppangm3 <hoppangm3@gmail.com>
2026-06-25 09:30:29 +0900

    fix: clean destination folder to prevent CodeDeploy overwrite error

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 echo "Installing Java 17..."
 sudo yum install -y java-17-amazon-corretto lsof
+
+echo "Cleaning up destination directory..."
+sudo rm -rf /home/ec2-user/app/*
+sudo rm -rf /home/ec2-user/app/.[!.]* || true
