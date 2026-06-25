7198169d55f416dd27012cd9547648c7643e39d3
surin <surinrtf@gmail.com>
2026-06-25 13:21:24 +0900

    fix error

--- src/main/java/com/aivle/bookapp/config/WebConfig.java
+++ src/main/java/com/aivle/bookapp/config/WebConfig.java
@@ -13,7 +13,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(
+                                "http://localhost:5173",
+                                "http://user48-prod-website.s3-website-us-west-1.amazonaws.com"
+                            )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
